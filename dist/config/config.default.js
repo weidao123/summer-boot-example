7e5dc29e28954e16e6b3322ef9bb23c304129c6b
qi.tian <qi.tian@broadfun.cn>
2021-01-06 11:44:48 +0800

    更新版本

--- dist/config/config.default.js
+++ dist/config/config.default.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    port: 8081
+    port: 8080,
+    worker: 2,
+    log: {
+        size: "1mb",
+        name: "summer-boot-example.log"
+    }
 };
